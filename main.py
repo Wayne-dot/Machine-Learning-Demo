ef2a9b6ac2615228f9bd1d43396a2b0287d007d9
Wayne-dot <wayneting.wc@gmail.com>
2023-02-05 21:37:24 -0500

    last max score

--- main.py
+++ main.py
@@ -296,6 +296,7 @@ def draw_window(surface, grid, score=0, last_score=0):
 
     surface.blit(label, (sx+20, sy+160))
 
+
     #last score
     label = font.render("Highest score" + last_score, 1, (255, 255, 255))
     sx = top_left_x - 200
