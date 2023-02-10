41dd5f7eeb82c740c97d654ebbb182aef7ad7b11
Wayne-dot <wayneting.wc@gmail.com>
2023-02-10 02:16:58 -0500

    rename main.py, add new file

--- myself.py
+++ myself.py
@@ -1,17 +1,44 @@
 import pygame
 
+
 # Shape
 
 # define dimension
 
 
+def start(win):
+    going = False
+    # intital draw
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                win.fill((14, 174, 243))
+                pygame.time.delay(10000)
+
+
+
+
+
+
 pygame.font.init()
 
-# size will be set in dimension
+# size will be set in dimension, surface object
 win = pygame.display.set_mode(size=(500, 500))
 
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            win.fill((14, 174, 243))
+            pygame.time.delay(10000)
+
 # title
-pygame.display.set_caption("Tetris by Wayne")
+# pygame.display.set_caption("Tetris by Wayne")
 
 
 print(win)
