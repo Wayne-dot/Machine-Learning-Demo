f359c3d78967bf03f179dc608c8060d148845514
Wayne-dot <wayneting.wc@gmail.com>
2023-02-15 02:33:09 -0500

    file update

--- neat.py
+++ neat.py
@@ -1,8 +1,7 @@
-print('helo')
 
 # Input = block, bottom information (both position and shape)
 # Output = change shape?
-# Activation function = tanh
+# Activation function = tanh, ranking how good it does from -1 to 1
 # Population size = how many running for each generation
-# Fitness function = score, how many row is remove
+# Fitness function = score, how many row is remove, to determine which one is better
 # Max generation = 30
